db4765da559ed88fb72aef0a23127ef825156242
Marc Mendoza <777marc@gmail.com>
2021-02-04 07:06:53 -0500

    menu animation

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,5 @@
 console.log('load main js')
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
@@ -10,5 +10,19 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleBtn);
 
 function toggleBtn() {
-    console.log('here')
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        showMenu = false;
+    }
 }
