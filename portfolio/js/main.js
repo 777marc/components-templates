a0c061872879888b985f6ac5ec399d9e7e4774a7
777marc@gmail.com <777marc@gmail.com>
2021-02-04 06:39:52 -0500

    button work

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,1 +1,14 @@
-console.log('main js');
+console.log('load main js')
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleBtn);
+
+function toggleBtn() {
+    console.log('here')
+}
